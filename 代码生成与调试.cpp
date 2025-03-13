ee1d9978d79fd557b2ceea48fe487e58771967a4
Inteygros <1755419418@qq.com>
2025-03-13 20:00:59 +0800

    second save

--- 代码生成与调试.cpp
+++ 代码生成与调试.cpp
@@ -37,6 +37,12 @@ int Difference_4(double delta_x, double x){
 
 int main()
 {
-
+    double x, delta_x, y1, y2, y3, y4;
+    scanf("输入自变量的值：%lf 网格间距：%lf", &x, &delta_x);
+    y1 = Difference_1(delta_x, x);//计算一阶导数两点向前差分
+    y2 = Difference_2(delta_x, x);//计算一阶导数三点向前差分
+    y3 = Difference_3(delta_x, x);//计算二阶导数三点向前差分
+    y4 = Difference_4(delta_x, x);//计算二阶导数中心差分
+    printf("一阶导数两点向前差分:%lf\n一阶导数三点向前差分:%lf\n二阶导数三点向前差分:%lf\n二阶导数中心差分:%lf", y1, y2, y3, y4);
     return 0;
 }
