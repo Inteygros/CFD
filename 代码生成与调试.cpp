18f6669f283a377e7d21a8ffd66e78d79c0c5db7
Inteygros <1755419418@qq.com>
2025-03-13 20:48:02 +0800

    third save

--- 代码生成与调试.cpp
+++ 代码生成与调试.cpp
@@ -1,35 +1,35 @@
 #include<cmath>
 #include<stdio.h>
 //定义函数F(x)
-int F_function(double x){
+double F_function(double x){
     double F;
     F = sin(x);
     return F;
 }
 
 //定义一阶导数两点向前差分
-int Difference_1(double delta_x, double x){
+double Difference_1(double delta_x, double x){
     double result;
     result = (F_function(x + delta_x) - F_function(x))/delta_x;
     return result;
 }
 
 //定义一阶导数三点向前差分
-int Difference_2(double delta_x, double x){
+double Difference_2(double delta_x, double x){
     double result;
     result = (-3 * F_function(x) + 4 * F_function(x + delta_x) - F_function(x + 2 * delta_x))/(2 * delta_x);
     return result;
 }
 
 //定义二阶导数三点向前差分
-int Difference_3(double delta_x, double x){
+double Difference_3(double delta_x, double x){
     double result;
     result = (F_function(x) - 2 * F_function(x + delta_x) + F_function(x + 2 * delta_x))/(delta_x * delta_x);
     return result;
 }
 
 //定义二阶导数中心差分
-int Difference_4(double delta_x, double x){
+double Difference_4(double delta_x, double x){
     double result;
     result = (F_function(x - delta_x) - 2 * F_function(x) + F_function(x + delta_x))/(delta_x * delta_x);
     return result;
@@ -38,7 +38,10 @@ int Difference_4(double delta_x, double x){
 int main()
 {
     double x, delta_x, y1, y2, y3, y4;
-    scanf("输入自变量的值：%lf 网格间距：%lf", &x, &delta_x);
+    printf("自变量值：");
+    scanf("%lf", &x);
+    printf("网格间距：");
+    scanf("%lf", &delta_x);
     y1 = Difference_1(delta_x, x);//计算一阶导数两点向前差分
     y2 = Difference_2(delta_x, x);//计算一阶导数三点向前差分
     y3 = Difference_3(delta_x, x);//计算二阶导数三点向前差分
