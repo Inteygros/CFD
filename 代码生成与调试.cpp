70454cf9e7c0491776b71d5be833f54f3ed092d3
Inteygros <1755419418@qq.com>
2025-03-16 16:14:57 +0800

    4th save

--- 代码生成与调试.cpp
+++ 代码生成与调试.cpp
@@ -3,33 +3,47 @@
 //定义函数F(x)
 double F_function(double x){
     double F;
-    F = sin(x);
+    F = sin(2 * x);//
+    return F;
+}
+
+//定义精确的一阶导函数
+double Difference1(double x){
+    double F;
+    F = 2 * cos(2 * x);//
+    return F;
+}
+
+//定义精确的二阶导函数
+double Difference2(double x){
+    double F;
+    F = -4 * sin(2 * x);// 
     return F;
 }
 
 //定义一阶导数两点向前差分
-double Difference_1(double delta_x, double x){
+double Difference1_1(double delta_x, double x){
     double result;
     result = (F_function(x + delta_x) - F_function(x))/delta_x;
     return result;
 }
 
 //定义一阶导数三点向前差分
-double Difference_2(double delta_x, double x){
+double Difference1_2(double delta_x, double x){
     double result;
     result = (-3 * F_function(x) + 4 * F_function(x + delta_x) - F_function(x + 2 * delta_x))/(2 * delta_x);
     return result;
 }
 
 //定义二阶导数三点向前差分
-double Difference_3(double delta_x, double x){
+double Difference2_1(double delta_x, double x){
     double result;
     result = (F_function(x) - 2 * F_function(x + delta_x) + F_function(x + 2 * delta_x))/(delta_x * delta_x);
     return result;
 }
 
 //定义二阶导数中心差分
-double Difference_4(double delta_x, double x){
+double Difference2_2(double delta_x, double x){
     double result;
     result = (F_function(x - delta_x) - 2 * F_function(x) + F_function(x + delta_x))/(delta_x * delta_x);
     return result;
@@ -37,15 +51,41 @@ double Difference_4(double delta_x, double x){
 
 int main()
 {
-    double x, delta_x, y1, y2, y3, y4;
+    double x, h, y1, y1_1, e1_1, p1_1, y1_2, e1_2, p1_2, y2, y2_1, e2_1, p2_1, y2_2, e2_2, p2_2;
     printf("自变量值：");
     scanf("%lf", &x);
-    printf("网格间距：");
-    scanf("%lf", &delta_x);
-    y1 = Difference_1(delta_x, x);//计算一阶导数两点向前差分
-    y2 = Difference_2(delta_x, x);//计算一阶导数三点向前差分
-    y3 = Difference_3(delta_x, x);//计算二阶导数三点向前差分
-    y4 = Difference_4(delta_x, x);//计算二阶导数中心差分
-    printf("一阶导数两点向前差分:%lf\n一阶导数三点向前差分:%lf\n二阶导数三点向前差分:%lf\n二阶导数中心差分:%lf", y1, y2, y3, y4);
+    printf("初始网格间距：");
+    scanf("%lf", &h);
+    int i = 1;
+    while(h > 10e-8){
+        y1 = Difference1(x);//计算精确的一阶导函数
+        y1_1 = Difference1_1(h, x);//计算一阶导数两点向前差分
+        y1_2 = Difference1_2(h, x);//计算一阶导数三点向前差分
+
+        y2 = Difference2(x);//计算精确的二阶导函数
+        y2_1 = Difference2_1(h, x);//计算二阶导数三点向前差分
+        y2_2 = Difference2_2(h, x);//计算二阶导数中心差分
+
+        if(i >1){
+            p1_1 = log2(fabs(e1_1)/fabs(y1_1-y1));
+            p1_2 = log2(fabs(e1_2)/fabs(y1_2-y1));
+            p2_1 = log2(fabs(e2_1)/fabs(y2_1-y2));
+            p2_2 = log2(fabs(e2_2)/fabs(y2_2-y2));//计算精度p
+        }
+
+        e1_1 = y1_1 - y1;//计算误差
+        e1_2 = y1_2 - y1;//计算误差
+        e2_1 = y2_1 - y2;//计算误差
+        e2_2 = y2_2 - y2;//计算误差
+        printf("当前循环数%d 网格间距:%lf\n", i, h);
+        printf("一阶差分1:%lf 一阶差分2:%lf 一阶导数:%lf\n二阶差分1:%lf 二阶差分2:%lf 二阶导数:%lf\n", y1_1, y1_2, y1, y2_1, y2_2, y2);
+        printf("一阶差分1误差:%lf 一阶差分2误差:%lf\n二阶差分1误差:%lf 二阶差分2误差:%lf\n", e1_1, e1_2, e2_1, e2_2);
+        if(i >1){
+            printf("一阶差分1精度:%lf 一阶差分2精度:%lf\n二阶差分1精度:%lf 二阶差分2精度:%lf\n\n", p1_1, p1_2, p2_1, p2_2);
+        }
+        h = h / 2;
+        i = i + 1;
+    }
+    
     return 0;
 }
